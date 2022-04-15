4ef3fe9a34f058591c9ca00416f93292ae7e61c5
Fernando Hernandez <fernandohruz@hotmail.com>
2022-04-14 22:29:08 -0500

    add react router to the webapp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { Link, Outlet, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ColumnsPreviewCard from './pages/ColumnsPreviewCard/ColumnsPreviewCard';
+import Home from './pages/Home/Home';
+import NavigationMenu from './shared-components/NavigationMenu/NavigationMenu';
 
 function App() {
   return (
     <div className="App">
-      <h1>I'm using react!</h1>
+      <NavigationMenu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/columns-preview-card' element={<ColumnsPreviewCard />} />
+      </Routes>
     </div>
   );
 }
