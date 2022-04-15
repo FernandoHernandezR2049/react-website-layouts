63067d4ea3792d620bf5f46c061a5ae1a22bd2fa
Fernando Hernandez <fernandohruz@hotmail.com>
2022-04-14 22:31:08 -0500

    add home page to the website

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css'
+const Home = () => {
+    return (
+        <div>
+            <h1 className="header">Welcome to my react layouts website!</h1>
+            <p>In this website you can navigate between some of my soulutions to frontend mentor challenges I've built with React.</p>
+        </div>
+
+
+    )
+}
+
+export default Home
