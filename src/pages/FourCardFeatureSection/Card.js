f6a7c6f388bb59e1f99a04c11c14f846545850cc
Fernando Hernandez <fernandohruz@hotmail.com>
2022-04-16 00:40:46 -0500

    add card component

---
+++ src/pages/FourCardFeatureSection/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = (props) => {
+    const cardStyle = { borderTopColor: props.borderColor }
+    return (
+        <article className="feature-card" style={cardStyle}>
+            <h3 className="feature-card-title">{props.cardTitle}</h3>
+            <p className="feature-card-text">{props.cardText}</p>
+            <img src={props.cardImage} alt='feature card' className="features-card-image" width='60px' />
+        </article>
+    )
+}
+export default Card;
